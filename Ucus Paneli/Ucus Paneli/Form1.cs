29e53d15e4afd68c3dcb63399f4cd68283a30539
Okan <73397121+Oknn88@users.noreply.github.com>
2020-12-10 20:28:58 +0300

    Travel-airplane-panel

---
+++ Ucus Paneli/Ucus Paneli/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucus_Paneli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Nereden: " + comboBox1.Text + " Nereye:" + comboBox2.Text + " Tarih:" + dateTimePicker1.Text + " Saat:" + maskedTextBox1.Text + " ~Yolcu Bilgileri~ Yolcu Ad Soyad:" + maskedTextBox2.Text + " Tc:" + maskedTextBox3.Text + " Telefon:" + maskedTextBox4.Text);
+        }
+    }
+}
